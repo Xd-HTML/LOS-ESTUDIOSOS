1ba61e2140b66db33ce482250a33719f17d11cde
Gustavo T. <gustavotellovillar@gmail.com>
2025-10-15 23:17:50 -0500

    app.js

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebas
 // 🔧 CONFIGURACIÓN DE TU PROYECTO FIREBASE
 // (Copia estos valores desde tu consola de Firebase > Configuración del proyecto > SDK web)
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO_ID",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TUSENDER_ID",
-  appId: "TU_APP_ID",
-  measurementId: "TU_MEASUREMENT_ID"
+  apiKey: "AIzaSyDwird5A7fTnSD3JA7HgHNJhVOi3yiPVwU",
+  authDomain: "stylish-steps.firebaseapp.com",
+  projectId: "stylish-steps",
+  storageBucket: "stylish-steps.firebasestorage.app",
+  messagingSenderId: "580730135694",
+  appId: "1:580730135694:web:3d77bfef3af246f9c755df",
+  measurementId: "G-21F9WH2PZT"
 };
 
 // 🚀 Inicializa Firebase
